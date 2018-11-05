1fb853ed3939d6c10eb3bccb3e04260fe6456b17
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2018-11-05 22:02:08 +0800

    Update python/rpcaTest.py

--- python/rpcaTest.py
+++ python/rpcaTest.py
@@ -4,7 +4,7 @@ import cv2
 import RPCA
 # def
 full_path = '/Users/wangyuxin/Movies/data'
-videos = ['fire.mov', 'water.mp4']
+videos = ['fire.mov']#, 'water.mp4']
 # pre
 def videos_path(videos):
   return map(
@@ -16,16 +16,19 @@ def videos_path(videos):
   )
 # func
 def run(path):
+  m, n = 192, 108
   # init
   capture = cv2.VideoCapture(path)
   data = None
   # run
   print('read {path}'.format(path=path))
+  nframes = 0
   while capture.isOpened():
     success, frame = capture.read()
     if not success:
       break
-    frame = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_LINEAR)
+    nframes += 1
+    frame = cv2.resize(frame, (m, n), interpolation=cv2.INTER_LINEAR)
     # gray
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = np.array(gray)
@@ -39,8 +42,20 @@ def run(path):
   # cal
   print('run')
   h = RPCA.rpcaADMM(data)
-  print(h)
   print('end {path}'.format(path=path))
+  # show
+  count = 0
+  while count < nframes:
+    X1 = h['X1_admm']
+    X2 = h['X2_admm']
+    X3 = h['X3_admm']
+    #
+    cv2.imshow('frame', X1[:, count].reshape((m, n)))
+    #
+    count += 1
+    print(count/nframes)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+      break
   # free
   cv2.destroyAllWindows()
 # run
