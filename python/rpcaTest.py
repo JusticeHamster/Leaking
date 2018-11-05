bb532b642c29588094ea1131380abcb783dd8bf8
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2018-11-05 23:31:17 +0800

    Update python/rpcaTest.py

--- python/rpcaTest.py
+++ python/rpcaTest.py
@@ -7,6 +7,9 @@ import RPCA
 """{rpca.settings 格式要求:}
 path=XXX
 videos=X.mp4;Y.mp4
+delay=?     // 视频播放延迟
+height=?    // 视频高度限定
+maxFrames=? // 取前N帧
 """
 settings = {}
 with open('rpca.settings', encoding='utf-8') as f:
@@ -19,21 +22,22 @@ videos = settings['videos']
 delay = int(settings.get('delay', 100))
   # 所需视频的高度
 height = int(settings.get('height', 192))
+maxFrames = int(settings.get('maxFrames', 100))
 # 将设置中的文件转换为绝对地址
 def videos_path(videos):
   return map(
-    lambda n: '{path}/{name}'.format(
+    lambda n: (n, '{path}/{name}'.format(
       path=full_path,
       name=n
-    ),
+    )),
     videos.split(';')
   )
 # 运行
-def run(path):
+def run(name, path):
   if not os.path.exists(path):
     print('no such file: {path}'.format(path=path))
     return
-  # ini
+  # init
   capture = cv2.VideoCapture(path)
   data = None
   # run
@@ -46,6 +50,8 @@ def run(path):
   m = height
   n = int(n * scale)
   while capture.isOpened():
+    if nframes >= maxFrames:
+      break
     success, frame = capture.read()
     if not success:
       break
@@ -77,14 +83,14 @@ def run(path):
       (X2[:, count].reshape(size)),
       (X3[:, count].reshape(size)),
     ))
-    cv2.imshow(path, picture)
+    cv2.imshow(name, picture)
     #
     count += 1
     cv2.waitKey(delay)
 # run
 if __name__ == '__main__':
-  for video in videos_path(videos):
-    run(video)
+  for name, video in videos_path(videos):
+    run(name, video)
     # wait
     input('<enter to continue>')
     # free
