ea7d75abcc1c8fc6b960ae9aa6b46a3964de8f5c
wuzhengyi <nju_wzy@163.com>
2018-11-19 21:19:08 +0800

    add param 'compression_ratio' in optical_flow.run_one_frame

--- python/optical_flow.py
+++ python/optical_flow.py
@@ -17,11 +17,25 @@ def draw_hsv(flow):
     return bgr
 
 
-def run_one_frame(prev, img, rect, color=(0, 0xFF, 0), thickness=4, limit_size=10):
+def run_one_frame(prev, img, rect, color=(0, 0xFF, 0), thickness=4, limit_size=10, compression_ratio=1):
     """
+    Params:
+        @prev: 上一帧图片
+        @img: 这一帧图片
+        @rect: 给定检测的范围
+        @color: 标识物体框的颜色
+        @thickness: 标识物体框的粗细
+        @limit_size: 小于此阈值的物体长宽，不予标识
+        @compression_ratio: 识别时的压缩率，越小检测结果越粗糙，速度越快
     Warning:
         rect: two points same as (x1,y1),(x2,y2), and x1 <= x2 & y1 <= y2
+
     """
+    src = img.copy()
+    prev = cv2.resize(prev.copy(), None, None,
+                      compression_ratio, compression_ratio)
+    img = cv2.resize(img.copy(), None, None,
+                     compression_ratio, compression_ratio)
     (x1, y1), (x2, y2) = rect
     prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -32,7 +46,7 @@ def run_one_frame(prev, img, rect, color=(0, 0xFF, 0), thickness=4, limit_size=1
     binary = cv2.threshold(gray1, 25, 0xFF, cv2.THRESH_BINARY)[1]
     # 对二值图像进行膨胀
     binary = cv2.dilate(binary, None, iterations=2)
-    cv2.imshow('binary', binary)
+
     _, cnts, _ = cv2.findContours(
         binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -44,26 +58,29 @@ def run_one_frame(prev, img, rect, color=(0, 0xFF, 0), thickness=4, limit_size=1
         if x < x1 or x > x2 or y < y1 or y > y2:
             continue
         if w > limit_size and h > limit_size:
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, thickness)
-    return img
+            cv2.rectangle(src, (np.int(x//compression_ratio), np.int(y//compression_ratio)), (np.int(
+                (x + w)//compression_ratio), np.int((y + h)//compression_ratio)), color, thickness)
+    return binary, src
 
 
 if __name__ == '__main__':
     import sys
-    screen_size = (270, 480)
-    rect = ((20,20),(250,460))
-    cam = cv2.VideoCapture('/Users/wzy/Movies/data/water1.mp4')
+    # screen_size = (270, 480)
+    rect = ((20, 20), (500, 900))
+    cam = cv2.VideoCapture('../video/water.mp4')
     success, prev = cam.read()
     if success:
-        prev = cv2.resize(prev, screen_size)
+        # prev = cv2.resize(prev, screen_size)
         while True:
             success, img = cam.read()
             if not success:
                 break
-            img = cv2.resize(img, screen_size)
-            ans = run_one_frame(prev, img.copy(),rect)
-            cv2.rectangle(ans, *rect, (0, 0xFF, 0), 2)
+            # img = cv2.resize(img, screen_size)
+            binary, ans = run_one_frame(
+                prev, img.copy(), rect, compression_ratio=0.6)
+            cv2.rectangle(ans, *rect, (0, 0, 0xFF), 2)
             cv2.imshow('Image', ans)
+            cv2.imshow('binary', binary)
             prev = img
             cv2.waitKey(10)
 
