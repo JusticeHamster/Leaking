7f1be87be00822848dda8a16de7f55fe6783d9a3
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2018-11-21 22:38:40 +0800

    Json5 update
    -> pip install json5

--- python/lktools/Loader.py
+++ python/lktools/Loader.py
@@ -1,62 +1,65 @@
+import json5
 import shutil
 import os
-# 读取设置
-"""{lk.settings 格式要求:}
-path=XXX
-videos=X.mp4;Y.mp4
-delay=?     // 视频播放延迟，默认100
-height=?    // 视频高度限定，宽度会自动计算，默认480
-frame_range=a-b// 取a-b帧，默认0-100
-img_path=?  // 图片存取路径，默认tmp
-video_path=?// 视频存取路径，默认tmp
-time_test=? // 是否测试时间，会关闭所有输出，默认false
-lastn=?     // 用前N帧图片作为修正的标准，默认为1
-fps=?       // 保存视频帧数，默认10
-time_debug=?// 是否打印每个函数耗时
-limit_size=?// 光流法的参数，默认10
-compression_ratio=? // 光流法的压缩率，默认1
-linux=?     // 是不是linux，linux不会执行显示相关的函数
-"""
 
-settings = None
+template = """{
+  "path": "../video",                     // 视频路径
+  "videos": ["???.mp4"],                  // 视频列表
+  "delay": 100,                           // 视频播放延迟，用于cv2.waitKey第一个参数
+  "height": 480,                          // 视频高度限定，宽度会自动计算
+  "frame_range": [0, 100],                // 取[a, b]帧
+  "img_path": "tmp",                      // 图片存取路径
+  "video_path": "tmp",                    // 视频存取路径
+  "time_test": false,                     // 是否测试时间，会关闭所有输出
+  "lastn": 1,                             // 用前N帧图片作为修正的标准
+  "fps": 10,                              // 保存视频帧数
+  "time_debug": false,                    // 是否打印每个函数耗时
+  "limit_size": 10,                       // 光流法的参数
+  "compression_ratio": 1,                 // 光流法的压缩率
+  "linux": false                          // 是不是linux，linux不会执行显示相关的函数
+}"""
+user_settings = None
 
 def get_settings():
-  global settings
-  if settings is None:
-    settings = {}
-  else:
-    return settings
-  with open('lk.settings', encoding='utf-8') as f:
-    for line in f.readlines():
-      strs = line.split('=')
-      settings[strs[0].strip()] = strs[1].strip()
+  global user_settings
+  if user_settings is not None:
+    return user_settings
   # 设置
-  settings.setdefault('img_path', 'tmp')
-  settings.setdefault('video_path', 'tmp')
-  settings['delay'] = int(settings.get('delay', 100))
-  settings['height'] = int(settings.get('height', 480))
-  settings['lastn'] = int(settings.get('lastn', 1))
-  settings['fps'] = int(settings.get('fps', 10))
-  settings['limit_size'] = int(settings.get('limit_size', 10))
-  settings['compression_ratio'] = float(settings.get('compression_ratio', 1))
-  settings['frame_range'] = tuple(
-    map(lambda s: int(s), settings.get('frame_range', '0-100').split('-'))
-  )
-  settings['time_test'] = settings.get('time_test') == 'true'
-  settings['time_debug'] = settings.get('time_debug') == 'true'
-  settings['linux'] = settings.get('linux') == 'true'
+  with open('settings.json', encoding='utf-8') as f:
+    user_settings = json5.load(f)
+  default_settings = json5.loads(template)
+  for item in default_settings.items():
+    user_settings.setdefault(*item)
+  # 去掉路径最后的'/'
+  path = user_settings['path']
+  if path[-1] == '/':
+    path = path[:-1]
+    user_settings['path'] = path
   # 将设置中的文件转换为绝对地址
-  settings['videos'] = tuple(map(
+  user_settings['videos'] = tuple(map(
     lambda n: (n.split('.')[0], '{path}/{name}'.format(
-      path=settings['path'],
+      path=path,
       name=n
     )),
-    settings['videos'].split(';')
+    user_settings['videos']
   ))
+  # 测试的情况下该返回了
+  if __name__ == '__main__':
+    return user_settings
   # 清空输出文件夹
-  img_path = settings['img_path']
-  if not settings['time_test']:
+  if not user_settings['time_test']:
+    img_path = user_settings['img_path']
     if os.path.exists(img_path):
       shutil.rmtree(img_path)
     os.mkdir(img_path)
-  return settings
+    video_path = user_settings['video_path']
+    # img和video在一个路径下就不重复做了
+    if img_path != video_path:
+      if os.path.exists(video_path):
+        shutil.rmtree(video_path)
+      os.mkdir(video_path)
+  return user_settings 
+
+if __name__ == '__main__':
+  settings = get_settings()
+  print(settings)
