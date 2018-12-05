5ce1549e119498c99f5b88d88cb9b8b1baac9c1d
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2018-12-05 21:09:06 +0800

    Create App.py

---
+++ python/App.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+kv = Builder.load_string('''
+Button:
+  text: "I was created by kv codes"
+''')
+
+class TestApp(App):
+  def build(self):
+    return kv
+
+TestApp().run()
