8ce4132c2a08565cf2957b1e15db33f2a44a1ec3
wuzhengyi <wuzy.nju@smail.com>
2018-12-24 01:46:06 +0800

    label标签为什么不对齐？另外将pizza像素用dp表示

--- python/GUI/BSOFGraph.py
+++ python/GUI/BSOFGraph.py
@@ -3,6 +3,7 @@ kivy
 """
 import kivy.uix.widget
 import kivy.garden.pizza
+from kivy.metrics import dp
 
 class BSOFGraph(kivy.uix.widget.Widget):
   """
@@ -16,9 +17,9 @@ class BSOFGraph(kivy.uix.widget.Widget):
     return kivy.garden.pizza.Pizza(
       serie=kwargs.get('serie'),
       legend_color=kwargs.get('legend_color', '000000'),
-      legend_value_rayon=kwargs.get('legend_value_rayon', 160),
-      legend_title_rayon=kwargs.get('legend_title_rayon', 320),
-      chart_size=kwargs.get('chart_size', 512),
-      chart_border=kwargs.get('chart_border', 2),
+      legend_value_rayon=kwargs.get('legend_value_rayon', dp(80)),
+      legend_title_rayon=kwargs.get('legend_title_rayon', dp(160)),
+      chart_size=kwargs.get('chart_size', dp(256)),
+      chart_border=kwargs.get('chart_border', dp(1)),
       font_name=kwargs.get('font_name', 'resources/fonts/msyh'),
     )
