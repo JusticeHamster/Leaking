9e2b46c54f2c27c40745b1918de5eb2509622e1e
RailgunHamster <railgunhamster@qq.com>
2019-02-25 16:12:34 +0800

    pizza

--- python/GUI/BSOFGraph.py
+++ python/GUI/BSOFGraph.py
@@ -31,3 +31,12 @@ class BSOFGraph(kivy.uix.widget.Widget):
     """
     serie: ( ('class', percentage, 'color'), ... )
     """
+    return kivy.garden.pizza.Pizza(
+      serie=kwargs.get('serie'),
+      legend_color=kwargs.get('legend_color', '000000'),
+      legend_value_rayon=kwargs.get('legend_value_rayon', dp(80)),
+      legend_title_rayon=kwargs.get('legend_title_rayon', dp(160)),
+      chart_size=kwargs.get('chart_size', dp(256)),
+      chart_border=kwargs.get('chart_border', dp(1)),
+      font_name=kwargs.get('font_name', 'resources/fonts/msyh'),
+    )
