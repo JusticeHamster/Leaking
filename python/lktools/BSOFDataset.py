1d952aece2641f0d4e9db624d54fa4bd4d9ebc72
RailgunHamster <railgunhamster@qq.com>
2019-04-03 15:32:07 +0800

    Update BSOFDataset.py

--- python/lktools/BSOFDataset.py
+++ python/lktools/BSOFDataset.py
@@ -3,15 +3,22 @@ import os
 
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
 
 class BSOFDataset(Dataset):
   """
     custom pytorch Dataset
+
+    path不为空
   """
   def __init__(self, path, size=(224, 224)):
+    if path[-1] == '/':
+      path = path[:-1]
+
     self.path = path
     self.size = size
 
+    self.type = os.path.split(path)[-1]
     self.files = os.listdir(path)
 
   def __getitem__(self, index):
@@ -20,8 +27,8 @@ class BSOFDataset(Dataset):
     img = cv2.resize(img, self.size)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = torch.from_numpy(img)
+    img = Variable(img, requires_grad=True)
     return img
 
   def __len__(self):
     return len(self.files)
-
